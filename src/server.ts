a95f67c6bb7690f961e5097cb626bccc0ec80a2f
leeedev <leeesangheee@gmail.com>
2021-05-28 15:50:47 +0900

    Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+    genre: String
+    createdAt: String
+    updatedAt: String
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): String
+    deleteMovie(id: Int!): String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => [],
+    movie: () => ({ id: 0, title: "hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) => "",
+    deleteMovie: (_, { id }) => "",
   },
 };
 
