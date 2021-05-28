c577997edeb3a99e7fe0683b11d397f2d8483526
leeedev <leeesangheee@gmail.com>
2021-05-28 16:56:29 +0900

    .env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({ schema });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}/`);
 });
