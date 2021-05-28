dccff393a82282fea8878e1a04ba3a62aa95de27
leeedev <leeesangheee@gmail.com>
2021-05-28 22:30:31 +0900

    Create Account 2

--- src/users/createAccount/createAccount.resolvers.ts
+++ src/users/createAccount/createAccount.resolvers.ts
@@ -1,16 +1,35 @@
 import client from "../../client";
+import bcrypt from "bcrypt";
 
 export default {
   Mutation: {
-    createAccount: (_, { firstName, lastName, username, email, password }) =>
-      client.user.create({
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          password,
-        },
-      }),
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      } finally {
+      }
+    },
   },
 };
