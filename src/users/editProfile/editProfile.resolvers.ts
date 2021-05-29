e8e28eb8e47d23512a897dafc19a596e7265aea5
leeedev <leeesangheee@gmail.com>
2021-05-29 23:33:15 +0900

    File Upload(temporary path, save received file to folder)

--- src/users/editProfile/editProfile.resolvers.ts
+++ src/users/editProfile/editProfile.resolvers.ts
@@ -1,11 +1,17 @@
 import * as bcrypt from "bcrypt";
 import { protectResolver } from "../users.utils";
+import { createWriteStream } from "fs";
 
 const resolverFn = async (
   _,
-  { firstName, lastName, username, email, password: newPassword },
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser, client }
 ) => {
+  const { filename, createReadStream } = await avatar;
+  const readStream = createReadStream();
+  const whiteStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(whiteStream);
+
   let uglyPassword = null;
   if (newPassword) uglyPassword = await bcrypt.hash(newPassword, 10);
 
