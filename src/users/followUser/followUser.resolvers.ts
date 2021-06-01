d84ffcf9fb7cd9fff6edf362f4dbe2cbf9ed8529
leeedev <leeesangheee@gmail.com>
2021-06-02 04:12:46 +0900

    Follow User

--- src/users/followUser/followUser.resolvers.ts
+++ src/users/followUser/followUser.resolvers.ts
@@ -1,8 +1,15 @@
 import { Resolvers } from "../../types";
 import { protectResolver } from "../users.utils";
 
-const resolverFn = async (_, { username }, { client }) => {
+const resolverFn = async (_, { username }, { loggedInUser, client }) => {
   try {
+    const user = await client.user.update({
+      where: { id: loggedInUser.id },
+      data: { following: { connect: { username } } },
+    });
+    if (!user) return { ok: false, error: "Cannot follow user." };
+
+    return { ok: true };
   } catch {
     return { ok: false, error: "Cannot follow user:(" };
   }
