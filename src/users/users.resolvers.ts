8b359461b7b78ff7fddcc4dbb8cc0db6719d0c90
leeedev <leeesangheee@gmail.com>
2021-06-02 16:28:47 +0900

    Computed Fields(isFollowing, isMe)

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -1,14 +1,19 @@
 import { Resolvers } from "../types";
-import client from "../client";
 
 const resolvers: Resolvers = {
   User: {
-    totalFollowers: async ({ id }) =>
+    totalFollowers: async ({ id }, _, { client }) =>
       await client.user.count({ where: { following: { some: { id } } } }),
-    totalFollowing: async ({ id }) =>
+    totalFollowing: async ({ id }, _, { client }) =>
       await client.user.count({ where: { followers: { some: { id } } } }),
-    isFollowing: async () => false,
-    isMe: async () => true,
+    isFollowing: async ({ id }, _, { loggedInUser, client }) => {
+      const exists = await client.user.count({
+        where: { id: loggedInUser.id, following: { some: { id } } },
+      });
+      return Boolean(exists);
+    },
+
+    isMe: async ({ id }, _, { loggedInUser }) => id === loggedInUser?.id,
   },
 };
 
