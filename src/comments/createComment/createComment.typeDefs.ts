09d39bef9181a9284fd23c788a119ffd1ddcfe92
leeedev <leeesangheee@gmail.com>
2021-06-15 23:44:24 +0900

    shared MutationResponse

--- src/comments/createComment/createComment.typeDefs.ts
+++ src/comments/createComment/createComment.typeDefs.ts
@@ -1,11 +1,7 @@
 import { gql } from "apollo-server-core";
 
 export default gql`
-  type createCommentResult {
-    ok: Boolean!
-    error: String
-  }
   type Mutation {
-    createComment(photoId: Int!, payload: String!): createCommentResult!
+    createComment(photoId: Int!, payload: String!): MutationResponse!
   }
 `;
