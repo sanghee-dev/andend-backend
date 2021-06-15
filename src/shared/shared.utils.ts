b39ed05cf831f363b200f46b0a1da687ba734f85
leeedev <leeesangheee@gmail.com>
2021-06-16 00:50:11 +0900

    S3 Photo Upload(upload photo on aws)

--- src/shared/shared.utils.ts
+++ src/shared/shared.utils.ts
@@ -6,3 +6,22 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_KEY,
   },
 });
+
+export const uploadToAWS = async (
+  file: any,
+  userId: number,
+  folderName: string
+) => {
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: "andend",
+      Key: objectName,
+      ACL: "public-read",
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
