ffc938149c59108535980bdcf59da520522fbbb0
leeedev <leeesangheee@gmail.com>
2021-06-23 17:24:38 +0900

    Update seeFeed.resolvers.ts

--- src/photos/seeFeed/seeFeed.resolvers.ts
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -1,23 +1,18 @@
 import { protectResolver } from "../../users/users.utils";
 import { Resolvers } from "../../types";
 
-const resolverFn = async (_, { offset }, { loggedInUser, client }) => {
-  try {
-    return client.photo.findMany({
-      where: {
-        OR: [
-          { user: { followers: { some: { id: loggedInUser.id } } } },
-          { userId: loggedInUser.id },
-        ],
-      },
-      orderBy: { createdAt: "desc" },
-      take: 2,
-      skip: offset,
-    });
-  } catch {
-    return [];
-  }
-};
+const resolverFn = async (_, { offset }, { loggedInUser, client }) =>
+  client.photo.findMany({
+    where: {
+      OR: [
+        { user: { followers: { some: { id: loggedInUser.id } } } },
+        { userId: loggedInUser.id },
+      ],
+    },
+    orderBy: { createdAt: "desc" },
+    take: 2,
+    skip: offset,
+  });
 
 const resolvers: Resolvers = {
   Query: {
